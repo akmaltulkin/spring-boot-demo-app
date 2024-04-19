27c10e112e0aaed5c7246225f96829d6e8fc80c1
Akmal <akmaltulkin@gmail.com>
2024-04-19 14:11:14 -0400

    intial commit 1

--- src/main/java/com/example/springbootdemoapp/SpringBootDemoAppApplication.java
+++ src/main/java/com/example/springbootdemoapp/SpringBootDemoAppApplication.java
@@ -1,13 +1,7 @@
 package com.example.springbootdemoapp;
 
-import com.example.springbootdemoapp.entitites.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
 
 @SpringBootApplication
 
