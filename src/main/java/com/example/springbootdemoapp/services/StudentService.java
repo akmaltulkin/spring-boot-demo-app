d1e43d4534fdd43458eb401f7998d46f566d514f
Akmal <akmaltulkin@gmail.com>
2024-04-19 15:45:22 -0400

    basic structure

--- src/main/java/com/example/springbootdemoapp/services/StudentService.java
+++ src/main/java/com/example/springbootdemoapp/services/StudentService.java
@@ -17,13 +17,13 @@ import java.util.List;
 public class StudentService {
 
 
-    public static List<Student> get100RandomStudents() {
+    public List<Student> get100RandomStudents() {
         List<Student> students = new ArrayList<>();
         Faker faker = new Faker();
         for (int i = 0; i < 100; i++) {
             //create a student with random data
             students.add(new Student(
-                    Long.valueOf(faker.number().numberBetween(1,1000)),
+                    (long) faker.number().numberBetween(1, 1000),
                     faker.name().fullName(),
                     faker. date().birthday(18,67).toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
                     faker.internet().emailAddress()
